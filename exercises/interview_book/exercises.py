e2555c59972e6fce905de354b5d4b42368c830dd
Ian Gilmore <gilmore.ian@gmail.com>
2014-09-16 19:50:30 -0700

    updated from mac 9/16/2014

--- exercises/interview_book/exercises.py
+++ exercises/interview_book/exercises.py
@@ -10,6 +10,7 @@ def check_unique(word):
             return letter + " is not unique in " + word
     return "No non-unique letters in " + word
 
+
 def generate_letter_array(word):
     letter_hash = {}
     for letter in word:
@@ -29,6 +30,7 @@ def compare_strings(first, second):
     else:
         return first + " is not a variant of " + second
 
+
 def replace_blank(word):
     # Exercise 1.4
     word_list = []
@@ -47,7 +49,7 @@ def compress_string(word):
     new_word = []
     for letter in word:
         if letter == last_letter:
-            count = count + 1
+            count += 1
         else:
             new_word.append(last_letter)
             new_word.append(str(count))
@@ -61,25 +63,29 @@ def compress_string(word):
     else:
         return "".join(new_word)
 
+
 class NodeSingle(object):
-    def __init__(self,data,next_node=None):
+    """Node for a singly linked list."""
+    def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
+
 
 def delete_dup_node(start_node):
     value_list = {}
     current_node = start_node
-    while current_node.next_node != None:
+    while current_node.next_node:
         temp_node = current_node.next_node
         if current_node.value in value_list:
             previous_node.next_node = temp_node
-            del(current_node)
+            del current_node
         else:
             value_list[current_node.value] = 1
             previous_node = current_node
         current_node = temp_node
 
-def return_kth_from_end(start_node,k):
+
+def return_kth_from_end(start_node, k):
     count = 0
     current_node = start_node
     return_node = start_node
@@ -89,3 +95,71 @@ def return_kth_from_end(start_node,k):
             return_node = return_node.next_node
         count += 1
     return return_node.value
+
+
+def add_lists(list1,list2):
+    """takes two numbers stored in linked lists in single digits, reverse order, adds them and returns the result in
+        a third linked list."""
+    result_list = NodeSingle(0)
+    current_list1 = list1
+    current_list2 = list2
+    current_result = result_list
+    done_list1 = False
+    done_list2 = False
+    carry = 0
+    while (done_list1 == False or done_list2 == False):
+        if done_list1 == True:
+            value1 = 0
+        else:
+            value1 = current_list1.data
+        if done_list2 == True:
+            value2 = 0
+        else:
+            value2 = current_list1.data
+        (carry, new_value) = divmod(value1 + value2 + carry, 10)
+        current_result.data = new_value
+        current_result.next_node = NodeSingle(0)
+        current_result = current_result.next_node
+        if current_list1.next_node:
+            current_list1 = current_list1.next_node
+        else:
+            done_list1 = True
+        if current_list2.next_node:
+            current_list2 = current_list2.next_node
+        else:
+            done_list2 = True
+    if carry == 1:
+        current_result.data = carry
+    else:
+        del current_result
+    return result_list
+
+def find_loop(start_node):
+    """Find if a linked list has a loop."""
+    slow_node = start_node
+    fast_node = start_node
+    while (slow_node.next_node and fast_node.next_node.next_node):
+        slow_node = slow_node.next_node
+        fast_node = fast_node.next_node.next_node
+        if slow_node == fast_node:
+            return True
+    return False
+
+
+def find_loop_start(start_node):
+    """Find first node of loop in list."""
+    slow_node = start_node
+    fast_node = start_node
+    in_loop = False
+    while (1):
+        slow_node = slow_node.next_node
+        fast_node = fast_node.next_node.next_node
+        if slow_node == fast_node:
+            if not in_loop:
+                in_loop = True
+                slow_node = start_node
+            else:
+                return slow_node
+        
+
+
