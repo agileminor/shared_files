5b8b574447e36de52ae02079489e21f676fbc3f4
Ian Gilmore <gilmore.ian@gmail.com>
2014-10-08 07:45:10 -0700

    Mac sync 10/7/2014, 1

--- exercises/minesweep.py
+++ exercises/minesweep.py
@@ -7,7 +7,13 @@ from code_jam_io import write_output
 num_cases, contents = read_input()
 output = []
 for i in range(1,num_cases+1):
-    results = 0.0
+    R = int(contents[i][0])
+    C = int(contents[i][1])
+    M = int(contents[i][2])
+    full_grid = [['*'] * R] * C
+    empty_cells = R*C-M-1
+    if R == 1 or C == 1:
+
     #    print i, finish_time, farm_time, prev_comp_time, comp_time, cum_time, counter
     output.append("Case #" + str(i) + ": %.7f" % results)
 
